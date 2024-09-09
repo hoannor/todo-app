ad149edab5788c11087709723de8c4c84a80b3ff
hoannor <halong4399@gmail.com>
2024-09-09 17:55:45 +0700

    sua lan 4(update)!

--- src/main.py
+++ src/main.py
@@ -23,12 +23,13 @@ Base = declarative_base()
 class todoItem(Base):
     __tablename__ = 'todos'
 
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key = True, nullable = False)
     title = Column(String, nullable = False)
     description = Column(String)
     completed = Column(Boolean, default = False)
+    inprogress = Column(Boolean, default = False)
 
-# tao bang trong co so du lieu (sao ham nay khong goi y)
+# tao bang trong co so du lieu
 Base.metadata.create_all(engine)
 
 # tao 1 Session de lam viec voi co so du lieu
@@ -39,6 +40,7 @@ class todoItemInput(BaseModel):
     title: str
     description: str = None
     completed: bool = False
+    inprogress: bool = False
 
 # Dinh nghia Pydantic model de dung lam response model
 class todoItemResponse(BaseModel):
@@ -46,11 +48,18 @@ class todoItemResponse(BaseModel):
     title: str
     description: str = None
     completed: bool = False
+    inprogress: bool = False
 
     class Config:
         orm_mode = True
 
 #note: chuyen tat cac cac du lieu tra ve (response_model cua cac end-point thanh kieu du lieu todoItemResponse thuoc kieu base model de co the tra ve duoi dang JSON cho trang web)
+
+# # End-point 0: trang chu
+# @app.get("/")
+# def root():
+#     return {"message": "Hello World"}
+
 # End-point 1: lay du lieu tu tat ca cac to-do
 @app.get("/todos", response_model = List[todoItemResponse])
 async def get_todos():
@@ -63,7 +72,7 @@ async def get_todos():
 @app.post("/todos", response_model = todoItemResponse)
 async def create_todo(todo: todoItemInput):
     session = SessionLocal()
-    tg = todoItem(title = todo.title, description = todo.description, completed = todo.completed)
+    tg = todoItem(title = todo.title, description = todo.description, completed = todo.completed, inprogress = todo.inprogress)
     session.add(tg)
     session.commit()
     session.refresh(tg)
@@ -82,16 +91,37 @@ async def delete_todo(todo_id: int):
         raise HTTPException(status_code = 404, detail = "todo not found")
 
 # End-point 4: thanh doi trang thai completed cua 1 to-do
-@app.patch("/todos/{todo_id}", response_model = todoItemResponse)
-async def update_todo(todo_id: int):
+@app.patch("/todos/completed/{todo_id}", response_model = todoItemResponse)
+async def update_todo_completed(todo_id: int):
     session = SessionLocal()
     todo_to_update = session.query(todoItem).filter(todoItem.id == todo_id).first()
     if todo_to_update is None:
         raise HTTPException(status_code = 404, detail = "todo not found!")
-    todo_to_update['completed'] = not todo_to_update['completed']
+    todo_to_update.completed = not todo_to_update.completed
     session.commit()
     session.refresh(todo_to_update)
     return todo_to_update
 
+# End-point 5: danh dau 1 todo la dang trong qua trinh lam
+@app.patch("/todos/in_progress/{todo_id}", response_model = todoItemResponse)
+async def update_todo_in_progress(todo_id: int):
+    session = SessionLocal()
+    todo_to_update = session.query(todoItem).filter(todoItem.id == todo_id).first()
+    if todo_to_update is None:
+        raise HTTPException(status_code = 404, detail = "todo not found!")
+    todo_to_update.in_progress = not todo_to_update.inprogress
+    session.commit()
+    session.refresh(todo_to_update)
+    return todo_to_update
 
+# End-point 6: chuc nang xoa cac todo da hoan thanh
+@app.patch("/todos/deletedones")
+async def delete_dones():
+    session = SessionLocal()
+    todo_to_delete = session.query(todoItem).filter(todoItem.completed == True).first()
+    while todo_to_delete is not None:
+        session.delete(todo_to_delete)
+        session.commit()
+        todo_to_delete = session.query(todoItem).filter(todoItem.completed == True).first()
+    return {"message": "All dones have been deleted"}
 
